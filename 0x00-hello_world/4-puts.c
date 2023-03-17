22796e36e3912575179c09cef7eed23156f9c84d
officialheartwell <officialheartwell@gmail.com>
2023-03-16 20:48:18 -0700

    fix task 4 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is a main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
