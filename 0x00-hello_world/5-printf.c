9561c52df5c00ef0882c92f6859c1a231d32fa97
officialheartwell <officialheartwell@gmail.com>
2023-03-16 20:58:18 -0700

    fix task 5 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is a function that which performs an action
+ *
+ * Return: 0 this returns 0 when sucessful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
