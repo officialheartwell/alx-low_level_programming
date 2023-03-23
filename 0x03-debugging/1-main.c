220b724cb2a9fe1673d10c13021aa891dd502942
officialheartwell <officialheartwell@gmail.com>
2023-03-23 05:15:10 -0700

    fix a bug on a file 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,8 +13,8 @@ int main(void)
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
-	/*putchar(i);*/
+	/*	putchar(i);*/
 	/*}*/
-	printf("infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
