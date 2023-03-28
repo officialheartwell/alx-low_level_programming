b3a3f79ac421f3e4cf09d0a3f82899512f971ad4
officialheartwell <officialheartwell@gmail.com>
2023-03-28 10:04:52 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * reset_to_98 - changes the value of n to 98
- *  @n: int to check
- * return: 0
+ * reset_to_98 - A function that take a pointer to an int as a parameter
+ * and updates the value its pointer to 98
+ * @n: input
+ * Return: n
  */
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
